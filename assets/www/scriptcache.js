34453ac903bdc84fcbe9f72b0fc145ba5dce9da0
Boris Smus <boris@borismus.com>
2010-11-16 12:16:01 +0000

    a bit of comments

--- assets/www/scriptcache.js
+++ assets/www/scriptcache.js
@@ -1,95 +1,50 @@
-var ScriptCache = (function() {
-  function ScriptCache() {
-    this.cache = {};
-    
-    var scriptCache = this;
-    $('[data-role=page]').live('pageshow', function(event) {
-      var pageName = event.target.id;
+// Whenever <page>.html is loaded, this cache loads <page>.js
 
-      // pageName might have some ? parameters tagged on. If so, remove the argument
-      var questionIndex = pageName.indexOf('?');
-      if (questionIndex != -1) {
-        pageName = pageName.substr(0, questionIndex);
-      }
-      if (pageName == '') {
-        return;
-      }
-      var jsName = pageName.replace(/\.[^\.]*$/,'') + '.js';
-      scriptCache.load(jsName);
-    });
-  };
+function ScriptCache() {
+  this.cache = {};
   
-  ScriptCache.prototype.load = function(src) {
-    var cache = this.cache;
-    if (cache[src] != undefined) {
-      // the script has already been loaded. don't load it again!
+  var scriptCache = this;
+  // Cause the external javascript to be loaded alongside the HTML
+  $('[data-role=page]').live('pageshow', function(event) {
+    var pageName = event.target.id;
+
+    // pageName might have some ? parameters tagged on. If so, remove the argument
+    var questionIndex = pageName.indexOf('?');
+    if (questionIndex != -1) {
+      pageName = pageName.substr(0, questionIndex);
+    }
+    if (pageName == '') {
       return;
     }
-    $.ajax({
-      dataType: 'script',
-      async: false,
-      cache: true,
-      url: src,
-      success: function() {
-        cache[src] = true;
-      }
-    });
-  };
-  
-  ScriptCache.prototype.onPageLoad = function(page, callback) {
-    $('[data-role=page]').live('pageshow', function(event) {
-      if (event.target.id.indexOf(page) == 0) {
-        callback();
-      }
-    });
-  };
-  
-  return ScriptCache;
-})();
+    var jsName = pageName.replace(/\.[^\.]*$/,'') + '.js';
+    scriptCache.load(jsName);
+  });
+};
 
-// var scriptCache = {
-//   cache: {},
-//   
-//   load: function(src) {
-//     var cache = this.cache;
-//     if (cache[src] != undefined) {
-//       // the script has already been loaded. don't load it again!
-//       return;
-//     }
-//     $.ajax({
-//       dataType: 'script',
-//       async: false,
-//       cache: true,
-//       url: src,
-//       success: function() {
-//         cache[src] = true;
-//       }
-//     });
-//   },
-//   
-//   onPageLoad: function(page, callback) {
-//     $('[data-role=page]').live('pageshow', function(event) {
-//       if (event.target.id.indexOf(page) == 0) {
-//         callback();
-//       }
-//     });
-//   },
-// 
-//   initialize: function() {
-// 
-//     $('[data-role=page]').live('pageshow', function(event) {
-//       var pageName = event.target.id;
-// 
-//       // pageName might have some ? parameters tagged on. If so, remove the argument
-//       var questionIndex = pageName.indexOf('?');
-//       if (questionIndex != -1) {
-//         pageName = pageName.substr(0, questionIndex);
-//       }
-//       if (pageName == '') {
-//         return;
-//       }
-//       var jsName = pageName.replace(/\.[^\.]*$/,'') + '.js';
-//       scriptCache.load(jsName);
-//     });
-//   }
-// };
+// Actually load the javascript (if not yet loaded) and cache it.
+ScriptCache.prototype.load = function(src) {
+  var cache = this.cache;
+  if (cache[src] != undefined) {
+    // the script has already been loaded. don't load it again!
+    return;
+  }
+  $.ajax({
+    dataType: 'script',
+    async: false,
+    cache: true,
+    url: src,
+    success: function() {
+      cache[src] = true;
+    }
+  });
+};
+
+// A way of registering a callback whenever the page loads. 
+// Each page has to do this for its own init method.
+ScriptCache.prototype.onPageLoad = function(page, callback) {
+  $('[data-role=page]').live('pageshow', function(event) {
+    if (event.target.id.indexOf(page) == 0) {
+      callback();
+    }
+  });
+};
